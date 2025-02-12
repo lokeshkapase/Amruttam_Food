3d6be187d4090ecc110322e12a6b9685f5d09b91
Lokesh <kapaselokesh@gmail.com>
2025-02-12 15:51:56 +0530

    Page 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.lokesh.amruttam_food"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lokesh.amruttam_food"
